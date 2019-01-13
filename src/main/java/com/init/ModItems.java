a90148916c6da7b3b12235a46ccab24ea4c6a0dc
David <1506368377@qq.com>
2019-01-13 17:22:33 +0800

    Item

--- src/main/java/com/init/ModItems.java
+++ src/main/java/com/init/ModItems.java
@@ -1,7 +1,10 @@
 package com.init;
 
 import com.items.ItemBase;
+import com.items.tools.ToolSword;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +12,21 @@ import java.util.List;
 public class ModItems {
 
     public static final List<Item> ITEMS = new ArrayList<Item>();
+    //Material
+
+    public static final Item.ToolMaterial AYNM = EnumHelper.addToolMaterial("AYNM",3,600,8.0F,3.0F,10);
+
+
+
+
+
+    //Items
     public static final Item COIN = new ItemBase("coin");
     public static final Item EMBLEM = new ItemBase("emblem");
     public static final Item MAGICCUBE = new ItemBase("magiccube");
     public static final Item REDDIMOND = new ItemBase("reddimond");
 
+    //Tools
+    public static final ItemSword AYANAMI_SWORD = new ToolSword("ayanami's sword",AYNM);
 
 }
